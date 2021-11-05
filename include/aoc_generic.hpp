13dc14aa76aeb1f2b76f9d41ed5cd6acc8a9c267
Hans-Martin <hmduringhof@gmail.com>
2021-11-05 21:16:58 +0100

    chore Increase consistency of lib usage

--- include/aoc_generic.hpp
+++ include/aoc_generic.hpp
@@ -1,5 +1,14 @@
+#include <algorithm>
 #include <fstream>
 #include <iostream>
+#include <sstream>
+#include <string>
 #include <vector>
 
 std::vector<int> readInput(std::string file);
+
+std::vector<std::string> readLines(std::string file);
+
+std::vector<std::string> split(const std::string &original, char delimiter = ' ', int maxSplits = -1);
+
+std::vector<std::string> split(const std::string &original, const std::string &delimiter, int maxSplits = -1);
--- lib/aoc_generic.cpp
+++ lib/aoc_generic.cpp
@@ -13,3 +13,54 @@ vector<int> readInput(string file){
 	inputFile.close();
 	return input;
 };
+
+vector<string> readLines(string file) {
+    // Reads input file and return a vector<string> called input
+    vector<string> lines;
+    ifstream inputFile(file);
+    string line;
+    while (getline(inputFile, line)){
+        lines.push_back(line);
+    }
+    inputFile.close();
+    return lines;
+}
+
+vector<string> split(const string &original, char delimiter, int maxSplits) {
+
+	vector<string> result;
+	string buffer;
+	istringstream source(original);
+	int splits = 0;
+
+	while (getline(source, buffer, delimiter) && splits != maxSplits)
+	{
+		result.push_back(buffer);
+		splits++;
+	}
+	return result;
+}
+
+vector<string> split(const string &original, const string &delimiter, int maxSplits) {
+	
+	vector<string> result;
+	int splits = 0;
+	size_t offset = 0;
+
+	while (true)
+	{
+		if (splits == maxSplits)
+		{
+			break;
+		}
+		size_t pos = original.find(delimiter, offset);
+		if (pos == string::npos)
+		{
+			break;
+		}
+		result.push_back(original.substr(offset, pos - offset));
+		offset = pos + delimiter.length();
+	}
+	result.push_back(original.substr(offset));
+	return result;
+}
