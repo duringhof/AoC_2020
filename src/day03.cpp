02d2015a0274aa7c9aa001d8ca1b73927d43ec44
Hans-Martin <hmduringhof@gmail.com>
2021-11-07 16:32:30 +0100

    feat Solution part 2 of day 3

--- src/day03.cpp
+++ src/day03.cpp
@@ -27,7 +27,12 @@ int countTrees(vector<vector<bool>> maze, int dx, int dy) {
 int main() {
 
     vector<vector<bool>> forest = readMaze("../input/day03.txt");
-    
+    vector<pair<int, int>> slopes = {{1,1}, {3,1}, {5,1}, {7,1}, {1,2}};
+    int productOfAnswers = 1;
+    for (auto slope : slopes) {
+        productOfAnswers = productOfAnswers*countTrees(forest, slope.first, slope.second);
+    }
 	cout << "The number of trees encountered is: " << countTrees(forest, 3, 1) << endl;
+    cout << "The product of the number of trees encountered in list of slopes is: " << productOfAnswers << endl;
     return 0;
 }
