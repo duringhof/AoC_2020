fae8a0de662ab8c8d15d8c79c9da8d6782dc3172
Hans-Martin <hmduringhof@gmail.com>
2021-11-10 22:17:52 +0100

    chore Preps for day 5

---
+++ src/day05.cpp
@@ -0,0 +1,12 @@
+
+#include "aoc_generic.hpp"
+
+using namespace std;
+
+int main() {
+
+    vector<string> boardingpasses = readLines("../input/day05.txt");
+    
+    cout << "Day 5 is not ready... " << endl;
+    return 0;
+}
