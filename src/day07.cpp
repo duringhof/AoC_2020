97cd2e67c18bfcce9f0a323dbcb3da24bd79110b
Hans-Martin <hmduringhof@gmail.com>
2021-11-13 18:42:13 +0100

    feat Preps for day 7

---
+++ src/day07.cpp
@@ -0,0 +1,11 @@
+#include "aoc_generic.hpp"
+
+using namespace std;
+
+int main() {
+
+    vector<string> rules = readLines("../input/day07.txt");
+
+    cout << "Day 7 is not ready..." <<  endl;
+    return 0;
+}
