99f15734be1cfc48cf967798430a9eca7fdc5319
Hans-Martin <hmduringhof@gmail.com>
2021-11-13 14:42:51 +0100

    feat Solution to day 6 part 1

--- src/day06.cpp
+++ src/day06.cpp
@@ -6,6 +6,27 @@ int main() {
 
     vector<string> lines = readLines("../input/day06.txt");
 
-    cout << "Day 6 is not finished" <<  endl;
+    vector<vector<char>> cdfs;    
+    vector<char> cdf;
+    int count = 0;
+    for(string line : lines) {
+        if(line.empty()) {
+            sort(cdf.begin(),cdf.end());
+            cdf.erase(unique(cdf.begin(),cdf.end()),cdf.end());
+            count = count + cdf.size();
+            cdfs.push_back(cdf);
+            cdf.clear();
+            continue;
+        }
+        for(char c : line) {
+            cdf.push_back(c);
+        }
+    }
+    sort(cdf.begin(),cdf.end());
+    cdf.erase(unique(cdf.begin(),cdf.end()),cdf.end());
+    count = count + cdf.size();
+    cdfs.push_back(cdf);
+
+    cout << "The sum of counts is: " << count << endl;
     return 0;
 }
