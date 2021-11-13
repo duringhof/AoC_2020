db05726a693efc09ab7b7a3d058227ed1bf14359
Hans-Martin <hmduringhof@gmail.com>
2021-11-13 14:14:00 +0100

    feat Preps for day 6

---
+++ src/day06.cpp
@@ -0,0 +1,11 @@
+#include "aoc_generic.hpp"
+
+using namespace std;
+
+int main() {
+
+    vector<string> lines = readLines("../input/day06.txt");
+
+    cout << "Day 6 is not finished" <<  endl;
+    return 0;
+}
