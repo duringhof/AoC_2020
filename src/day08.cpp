4c6b4667cae2528313ccb6a2064e9410c4da8c3b
Hans-Martin <hmduringhof@gmail.com>
2021-11-14 16:31:30 +0100

    feat Preps for day 8

---
+++ src/day08.cpp
@@ -0,0 +1,12 @@
+#include "aoc_generic.hpp"
+
+using namespace std;
+
+int main() {
+
+    vector<string> lines = readLines("../input/day08.txt");
+    
+    cout << "Day 8 is not finished" << endl;
+
+    return 0;
+}
