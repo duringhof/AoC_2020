5bcec3d654c05785afb4b73a6b997111b92ed8a9
Hans-Martin <hmduringhof@gmail.com>
2021-11-15 21:49:45 +0100

    feat Preps for day 10

---
+++ src/day10.cpp
@@ -0,0 +1,12 @@
+#include "aoc_generic.hpp"
+
+using namespace std;
+
+int main() {
+
+    vector<int> numbers = readInput("../input/day10.txt");
+    
+    cout << "Day 10 is not ready..." << endl;
+
+    return 0;
+}
