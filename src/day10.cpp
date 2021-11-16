fc45c1fb0fd23424337916b8497e4eab19866e0d
Hans-Martin <hmduringhof@gmail.com>
2021-11-16 08:21:39 +0100

    feat Solution day 10 part 1

--- src/day10.cpp
+++ src/day10.cpp
@@ -4,9 +4,32 @@ using namespace std;
 
 int main() {
 
-    vector<int> numbers = readInput("../input/day10.txt");
+    vector<int> ratings = readInput("../input/day10.txt");
+
+    sort(ratings.begin(), ratings.end());
+
+    vector<int> diffs(ratings.size()+1, 0);
+
+    diffs[0] = ratings[0];
+    for(int i = 1; i < ratings.size(); i++) {
+
+        diffs[i] = ratings[i] - ratings[i-1];
+    }
+    diffs[diffs.size()-1] = 3;
     
-    cout << "Day 10 is not ready..." << endl;
+    int ones = 0;
+    int threes = 0;
+
+    for(int i = 0; i < diffs.size(); i++) {
+
+        if (diffs[i] == 1) {
+            ones++;
+        } else if (diffs[i] == 3) {
+            threes++;
+        }
+    }
+
+    cout << "Part 1: The number of ones multiplied by the number of threes is:  " << ones * threes << endl;
 
     return 0;
 }
