69d4f3765b231f58aa5ce2a35a12c5752f5a47c4
Hans-Martin <hmduringhof@gmail.com>
2021-11-21 21:12:47 +0100

    feat Preps for day 12

---
+++ src/day12.cpp
@@ -0,0 +1,12 @@
+#include "aoc_generic.hpp"
+
+using namespace std;
+
+int main() {
+
+    vector<string> lines = readLines("../input/day02.txt");
+
+    cout << "Day 12 is not ready..." << endl;
+
+    return 0;
+}
