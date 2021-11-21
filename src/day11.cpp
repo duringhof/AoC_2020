639283e5eac5970b1f920df059d51220d233606f
Hans-Martin <hmduringhof@gmail.com>
2021-11-21 20:57:55 +0100

    feat Solution day 11 part 2

--- src/day11.cpp
+++ src/day11.cpp
@@ -79,15 +79,66 @@ int countOccupiedSeats(vector<vector<seat>> seatmap) {
     return count;
 }
 
+int countVisOccSeats(vector<vector<seat>>& seatmap, int x, int y) {
+    int count = 0;
+    int dist = 0;
+    for (int i = -1; i <= 1; i++) {
+        for (int j = -1; j <= 1; j++) {
+            if (i == 0 && j == 0) {
+                continue;
+            }
+            dist = 1;
+            while (x+i*dist >= 0 && x+i*dist < seatmap.size() &&
+                   y+j*dist >= 0 && y+j*dist < seatmap[0].size()) {
+                        if (seatmap[x + i*dist][y + j*dist].occupied) {
+                            count++;
+                            break;
+                        };
+                        if (seatmap[x + i*dist][y + j*dist].floor) {
+                            dist++;
+                        } else {
+                            break;
+                        }
+            }
+        }
+    }
+    return count;
+}
+
+vector<vector<seat>> swapSeatsB(vector<vector<seat>> oldseatmap) {
+    vector<vector<seat>> newseatmap = oldseatmap;
+    for (int i = 0; i < oldseatmap.size(); i++) {
+        for (int j = 0; j < oldseatmap[0].size(); j++) {
+            if (oldseatmap[i][j].floor) {
+                continue;
+            }
+            if (!oldseatmap[i][j].occupied && countVisOccSeats(oldseatmap, i, j) == 0) {
+                newseatmap[i][j].occupied = true;
+            }
+            if (oldseatmap[i][j].occupied && countVisOccSeats(oldseatmap, i, j) >= 5) {
+                newseatmap[i][j].occupied = false;
+            }
+        }
+    }
+    return newseatmap;
+}
+
 int main() {
 
     vector<vector<seat>> seatmap = readSeatMap("../input/day11.txt");
+    vector<vector<seat>> seatmapB = seatmap;
 
     while (countOccupiedSeats(swapSeats(seatmap)) != countOccupiedSeats(seatmap)) {
         seatmap = swapSeats(seatmap);
     }
 
     cout << "Part 1 - The number of occupied seats after stabilization is: " << countOccupiedSeats(seatmap) << endl;
+
+    while (countOccupiedSeats(swapSeatsB(seatmapB)) != countOccupiedSeats(seatmapB)) {
+        seatmapB = swapSeatsB(seatmapB);
+    }
+
+    cout << "Part 2 - The number of occupied seats after stabilization is: " << countOccupiedSeats(seatmapB) << endl;
 
     return 0;
 }
