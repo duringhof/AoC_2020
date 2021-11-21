b46dcc2310d63ad4cdcdcf3a96512e5cee1ed097
Hans-Martin <hmduringhof@gmail.com>
2021-11-21 09:44:44 +0100

    feat Solution day 10 part 2 (and refactored p1)

--- src/day10.cpp
+++ src/day10.cpp
@@ -2,34 +2,47 @@
 
 using namespace std;
 
+int countDiffs(vector<int> ratings, int diff) {
+    int count = 0;
+    for (int i = 1; i < ratings.size(); i++) {
+        if (ratings[i] - ratings[i-1] == diff) {
+            count++;
+        }
+    }
+    return count;
+}
+
+int mymin(int a, int b) {
+    return a < b ? a : b;
+}
+
+vector<long int> pathsToEnd(vector<int> ratings) {
+    vector<long int> answer(ratings.size(), 0);
+    answer[ratings.size()-1] = 1;
+    for (int i = ratings.size()-2; i >= 0; i--) {
+        for (int j = i+1; j < mymin(i+4, ratings.size()); j++) {
+            if (ratings[j] - ratings[i] <= 3) {
+                answer[i] += answer[j];
+            }
+        }
+    }
+    return answer;
+}
+
 int main() {
 
     vector<int> ratings = readInput("../input/day10.txt");
 
+    ratings.push_back(0);
     sort(ratings.begin(), ratings.end());
+    ratings.push_back(ratings.back()+3);
 
-    vector<int> diffs(ratings.size()+1, 0);
+    cout << "Part 1: The number of ones multiplied by the number of threes is:  "
+         << countDiffs(ratings, 1) * countDiffs(ratings,3) << endl;
 
-    diffs[0] = ratings[0];
-    for(int i = 1; i < ratings.size(); i++) {
-
-        diffs[i] = ratings[i] - ratings[i-1];
-    }
-    diffs[diffs.size()-1] = 3;
-    
-    int ones = 0;
-    int threes = 0;
-
-    for(int i = 0; i < diffs.size(); i++) {
-
-        if (diffs[i] == 1) {
-            ones++;
-        } else if (diffs[i] == 3) {
-            threes++;
-        }
-    }
-
-    cout << "Part 1: The number of ones multiplied by the number of threes is:  " << ones * threes << endl;
+    int start = 0;
+    cout << "Part 2: The number of possible combinations is: "
+         << pathsToEnd(ratings)[0] << endl;
 
     return 0;
 }
