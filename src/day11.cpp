47fa409c03658b1594276c7e78ec71b812ea9be2
Hans-Martin <hmduringhof@gmail.com>
2021-11-21 09:54:43 +0100

    feat Preps for day 11

---
+++ src/day11.cpp
@@ -0,0 +1,12 @@
+#include "aoc_generic.hpp"
+
+using namespace std;
+
+int main() {
+
+    vector<int> ratings = readInput("../input/day11.txt");
+
+    cout << "Day 11 is not done yet..." << endl;
+
+    return 0;
+}
