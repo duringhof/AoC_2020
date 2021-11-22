087a3991a1ce8522603a06e7c6d8c13197cd4fde
Hans-Martin <hmduringhof@gmail.com>
2021-11-22 10:30:27 +0100

    refactor Instr execution to function

--- src/day12.cpp
+++ src/day12.cpp
@@ -18,58 +18,62 @@ vector<Instruction> readInstructions(vector<string> lines) {
     return instructions;
 }
 
+void executeInstruction(Instruction instruction, int& x, int& y, char& facing) {
+    if (instruction.direction == 'N') {
+        y += instruction.distance;
+    } else if (instruction.direction == 'S') {
+        y -= instruction.distance;
+    } else if (instruction.direction == 'E') {
+        x += instruction.distance;
+    } else if (instruction.direction == 'W') {
+        x -= instruction.distance;
+    } else if (instruction.direction == 'L') {
+        for (int i = 0; i < instruction.distance/90; i++) {
+            if (facing == 'N') {
+                facing = 'W';
+            } else if (facing == 'W') {
+                facing = 'S';
+            } else if (facing == 'S') {
+                facing = 'E';
+            } else if (facing == 'E') {
+                facing = 'N';
+            }
+        }
+    } else if (instruction.direction == 'R') {
+        for (int i = 0; i < instruction.distance/90; i++) {
+            if (facing == 'N') {
+                facing = 'E';
+            } else if (facing == 'E') {
+                facing = 'S';
+            } else if (facing == 'S') {
+                facing = 'W';
+            } else if (facing == 'W') {
+                facing = 'N';
+            }
+        }
+    } else if (instruction.direction == 'F') {
+        if (facing == 'N') {
+            y += instruction.distance;
+        } else if (facing == 'S') {
+            y -= instruction.distance;
+        } else if (facing == 'E') {
+            x += instruction.distance;
+        } else if (facing == 'W') {
+            x -= instruction.distance;
+        }
+    }
+}
+
 int main() {
 
     vector<Instruction> instructions = readInstructions(readLines("../input/day12.txt"));
 
-    char currentDirection = 'E';
+    char facing = 'E';
     int x = 0;
     int y = 0;
 
     for (auto instruction : instructions) {
-        if (instruction.direction == 'N') {
-            y += instruction.distance;
-        } else if (instruction.direction == 'S') {
-            y -= instruction.distance;
-        } else if (instruction.direction == 'E') {
-            x += instruction.distance;
-        } else if (instruction.direction == 'W') {
-            x -= instruction.distance;
-        } else if (instruction.direction == 'L') {
-            for (int i = 0; i < instruction.distance/90; i++) {
-                if (currentDirection == 'N') {
-                    currentDirection = 'W';
-                } else if (currentDirection == 'W') {
-                    currentDirection = 'S';
-                } else if (currentDirection == 'S') {
-                    currentDirection = 'E';
-                } else if (currentDirection == 'E') {
-                    currentDirection = 'N';
-                }
-            }
-        } else if (instruction.direction == 'R') {
-            for (int i = 0; i < instruction.distance/90; i++) {
-                if (currentDirection == 'N') {
-                    currentDirection = 'E';
-                } else if (currentDirection == 'E') {
-                    currentDirection = 'S';
-                } else if (currentDirection == 'S') {
-                    currentDirection = 'W';
-                } else if (currentDirection == 'W') {
-                    currentDirection = 'N';
-                }
-            }
-        } else if (instruction.direction == 'F') {
-            if (currentDirection == 'N') {
-                y += instruction.distance;
-            } else if (currentDirection == 'S') {
-                y -= instruction.distance;
-            } else if (currentDirection == 'E') {
-                x += instruction.distance;
-            } else if (currentDirection == 'W') {
-                x -= instruction.distance;
-            }
-        }
+        executeInstruction(instruction, x, y, facing);
     }
 
     cout << "Part 1 - The Manhattan Distance between new and original location: "
