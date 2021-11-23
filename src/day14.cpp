791a04ff0b6e062f7af4894bebeb0ceec494a6b2
Hans-Martin <hmduringhof@gmail.com>
2021-11-23 08:38:43 +0100

    feat Preps for day 14

---
+++ src/day14.cpp
@@ -0,0 +1,19 @@
+#include "aoc_generic.hpp"
+
+using namespace std;
+
+struct Memory {
+    int address;
+    int value;
+};
+
+int main() {
+
+    vector<string> lines = readLines("../input/test.txt");
+
+    cout << "Part 1 - Not done yet... "
+         << endl
+         << "Part 2 - Not done yet... " << endl;
+
+    return 0;
+}
