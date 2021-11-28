2689f4284eefe75f528948d17c6afae715618335
Hans-Martin <hmduringhof@gmail.com>
2021-11-28 11:28:37 +0100

    feat Preps for day 15

---
+++ src/day15.cpp
@@ -0,0 +1,13 @@
+#include "aoc_generic.hpp"
+
+using namespace std;
+
+int main() {
+    vector<int> input = {11, 0, 1, 10, 5, 19};
+    
+    cout << "Part 1 - Not done yet... "
+         << 123456 << endl
+         << "Part 2 - Not done yet... " << endl;
+
+    return 0;
+}
